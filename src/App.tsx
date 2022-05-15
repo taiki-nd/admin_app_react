5c53eb22c1b18e73ad72ceb0193448aeb7640b34
taiki-nd <taiki.nd.real@gmail.com>
2022-05-15 22:08:32 +0900

    コンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
 import { Menu } from './components/Menu'
+import Users from './pages/Users';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -13,28 +15,8 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <Users />
+            <Dashboard />
           </main>
         </div>
       </div>
