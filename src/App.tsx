045a4f2d00b71ff03679d181576d0531872641ac
taiki-nd <taiki.nd.real@gmail.com>
2022-05-16 00:55:31 +0900

    wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
-import { Menu } from './components/Menu'
-import Users from './pages/Users';
-import Dashboard from './pages/Dashboard';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Nav />
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <Routes>
-              <Route path='/' element={<Dashboard />}/>
-              <Route path='/users' element={<Users />}/>
-            </Routes>
-          </main>
-        </div>
-      </div>
-    </div>
+      <Routes>
+        <Route path='/' element={<Dashboard />}/>
+        <Route path='/users' element={<Users />}/>
+        <Route path='/register' element={<Register />}/>
+      </Routes>
     </BrowserRouter>
   );
 }
