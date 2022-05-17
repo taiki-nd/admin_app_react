4f6fa34f90acdd8e5cdcf93b2cdfbc1bc6841f24
taiki-nd <taiki.nd.real@gmail.com>
2022-05-17 23:14:38 +0900

    ログイン処理

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,42 @@
+import axios from 'axios';
+import { SyntheticEvent } from 'react';
 import { useState } from 'react';
-import { text } from 'stream/consumers';
 
 const Login = () => {
 
-  const [text, setText] = useState("hello")
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const response = await axios.post('http://localhost:8000/api/login', {
+      email: email,
+      password: password,
+    }, {
+      withCredentials: true
+    });
+
+    console.log(response.data)
+  }
 
   return(
-    <div>
-      <h1>{text}</h1>
-      <input type={"text"} placeholder="some text" onChange={e => setText(e.target.value)} />
-    </div>
-  )
+    <main className="form-signin">
+        <form onSubmit={submit}>
+          <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+            <input type="email" className="form-control" placeholder="Email" required
+              onChange={e => setEmail(e.target.value)}
+            />
+
+            <input type="password" className="form-control" placeholder="Password" required
+              onChange={e => setPassword(e.target.value)}
+            />
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+        </form>
+    </main>
+  );
 }
 
 export default Login
