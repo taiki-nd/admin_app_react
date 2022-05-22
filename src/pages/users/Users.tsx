1ba5f11371d324af1cc28700439d70a94733aa2b
taiki-nd <taiki.nd.real@gmail.com>
2022-05-22 18:01:47 +0900

    role情報取得

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -39,7 +39,7 @@ export const Users = () => {
                   <td>{user.id}</td>
                   <td>{user.first_name} {user.last_name}</td>
                   <td>{user.email}</td>
-                  <td></td>
+                  <td>{user.role.name}</td>
                   <td></td>
                 </tr>
               )
