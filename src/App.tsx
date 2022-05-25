915b1e55bebc359a71f26d69656e20f8d27eb3f4
taiki-nd <taiki.nd.real@gmail.com>
2022-05-25 20:55:53 +0900

    ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import UserCreate from './pages/users/UserCreate';
 import UserUpdate from './pages/users/UserUpdate';
+import Roles from './pages/roles/roles';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/users/edit/:id' element={<UserUpdate />}/>
         <Route path='/register' element={<Register />}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='roles' element={<Roles/>}/>
       </Routes>
     </BrowserRouter>
   );
