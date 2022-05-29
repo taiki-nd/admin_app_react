b0e7132eaf495513a8b4e32bbf5b52902fc1013d
taiki-nd <taiki.nd.real@gmail.com>
2022-05-29 11:05:25 +0900

    削除機能

--- src/pages/products/produts.tsx
+++ src/pages/products/produts.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Wrapper from "../../components/Wrapper";
 import { Product } from "../../models/product";
 
@@ -20,6 +21,13 @@ const Products = () => {
     }
     getProducts();
   }, []);
+
+  const deleteProduct = async (id: number) => {
+    if (window.confirm(`Are you sure you  want to delete this User?: id = ${id}`)){
+      await axios.delete(`/users/${id}`);
+      setProducts(products.filter((p: Product) => p.id !== id));
+    }
+  }
 
   return (
     <Wrapper>
@@ -44,7 +52,12 @@ const Products = () => {
                   <td>{p.title}</td>
                   <td>{p.description}</td>
                   <td>{p.price}</td>
-                  <td></td>
+                  <td>
+                  <div className='btn-group mr-2'>
+                      <a href="/users" className='btn btn-sm btn-outline-danger m-1' onClick={() => deleteProduct(p.id)}>Delete</a>
+                      <Link to={`/users/edit/${p.id}`} className='btn btn-sm btn-outline-warning m-1'>Edit</Link>
+                    </div>
+                  </td>
                 </tr>
               )
             })}
